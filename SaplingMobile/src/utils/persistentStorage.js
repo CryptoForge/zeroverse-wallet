5521a5f0e8f105890910826e81244bf89cf10183
Cryptoforge <cryptoforge.cc@gmail.com>
2020-10-10 20:41:54 -0700

    comment out unused errorHandler

--- SaplingMobile/src/utils/persistentStorage.js
+++ SaplingMobile/src/utils/persistentStorage.js
@@ -81,29 +81,29 @@ export function writeDataToFile(fileEntry, data) {
 //   )
 // }
 
-const errorHandler = function (fileName, e) {
-  var msg = ''
-
-  switch (e.code) {
-    case FileError.QUOTA_EXCEEDED_ERR:
-      msg = 'Storage quota exceeded'
-      break
-    case FileError.NOT_FOUND_ERR:
-      msg = 'File not found'
-      break
-    case FileError.SECURITY_ERR:
-      msg = 'Security error'
-      break
-    case FileError.INVALID_MODIFICATION_ERR:
-      msg = 'Invalid modification'
-      break
-    case FileError.INVALID_STATE_ERR:
-      msg = 'Invalid state'
-      break
-    default:
-      msg = 'Unknown error'
-      break
-  }
-
-  alert('Error (' + fileName + '): ' + msg)
-}
+// const errorHandler = function (fileName, e) {
+//   var msg = ''
+//
+//   switch (e.code) {
+//     case FileError.QUOTA_EXCEEDED_ERR:
+//       msg = 'Storage quota exceeded'
+//       break
+//     case FileError.NOT_FOUND_ERR:
+//       msg = 'File not found'
+//       break
+//     case FileError.SECURITY_ERR:
+//       msg = 'Security error'
+//       break
+//     case FileError.INVALID_MODIFICATION_ERR:
+//       msg = 'Invalid modification'
+//       break
+//     case FileError.INVALID_STATE_ERR:
+//       msg = 'Invalid state'
+//       break
+//     default:
+//       msg = 'Unknown error'
+//       break
+//   }
+//
+//   alert('Error (' + fileName + '): ' + msg)
+// }
